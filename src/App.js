e1ae2f272f58a5c215f62f2d2fe039a79935092b
Aditya Naik <naik.aditya@gmail.com>
2018-12-24 15:58:59 +0100

    Compares if both selected characters are from same home world and shows the result

--- src/App.js
+++ src/App.js
@@ -5,11 +5,34 @@ class App extends Component{
 
     constructor(){
         super();
+        this.state = {
+            HomeWorld1: '',
+            HomeWorld2: '',
+            CompareHomeWorld: ''
+            }
+        this.HomeWorldCheck = this.HomeWorldCheck.bind(this)
 
     }
+
     HomeWorldCheck(HWName,id){
-        // console.log(HomeWorld1,HomeWorld2)
-        console.log(HWName,id)
+        id == 1 ?
+        this.setState({
+            HomeWorld1: HWName
+        }) :
+        this.setState({
+            HomeWorld2: HWName
+        })
+        if(this.state.HomeWorld1 && this.state.HomeWorld2){this.HomeWorldCompare()}
+    }
+
+    HomeWorldCompare(){
+        this.state.HomeWorld1 == this.state.HomeWorld2 ?
+        this.setState({
+            CompareHomeWorld: 'They are from Same Home World!'
+        }) :
+        this.setState({
+            CompareHomeWorld: 'They are from Different Home World!'
+        })
     }
     
     render(){
@@ -23,6 +46,12 @@ class App extends Component{
                 id = "2"
                 HWCheck = {this.HomeWorldCheck}
                 />
+                <span>
+                    HW1: {this.state.HomeWorld1}
+                    <br/>
+                    HW2: {this.state.HomeWorld2}
+                    <h2>{this.state.CompareHomeWorld}</h2>
+                </span>
             </div>
         )
     }
