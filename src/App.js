4ddd2d405bf5a6d4bbbd7ce3fe73ada107e42a4e
Aditya Naik <naik.aditya@gmail.com>
2019-01-07 21:41:56 +0100

    Adds conditional styling to result

--- src/App.js
+++ src/App.js
@@ -43,8 +43,8 @@ class App extends Component {
           <ApiComponent id="2" HWCheck={this.HomeWorldCheck} />
         </div>
 
-        <p style={comparisonStyle}>
-          <h2>{this.state.CompareHomeWorld}</h2>
+        <p style = {comparisonStyle}>
+          <h2 style={{ color: this.state.CompareHomeWorld === "They are from Same Home World!" ? "green" : "brown" }}>{this.state.CompareHomeWorld}</h2>
         </p>
       </div>
     );
@@ -56,6 +56,6 @@ const appStyle = {
   justifyContent: "center"
 };
 const comparisonStyle = {
-  textAlign: "center"
+  textAlign: "center",
 };
 export default App;
