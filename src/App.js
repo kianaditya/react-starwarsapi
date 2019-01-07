ede75e952789ffd482d382d8dd55f8dee498bb3e
Aditya Naik <naik.aditya@gmail.com>
2019-01-07 21:30:59 +0100

    Updates dependencies, adds styling to outer layout

--- src/App.js
+++ src/App.js
@@ -1,60 +1,61 @@
-import React,{Component} from 'react'
-import ApiComponent from './ApiComponent';
+import React, { Component } from "react";
+import ApiComponent from "./ApiComponent";
 
-class App extends Component{
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      HomeWorld1: "",
+      HomeWorld2: "",
+      CompareHomeWorld: ""
+    };
+    this.HomeWorldCheck = this.HomeWorldCheck.bind(this);
+  }
 
-    constructor(){
-        super();
-        this.state = {
-            HomeWorld1: '',
-            HomeWorld2: '',
-            CompareHomeWorld: ''
-            }
-        this.HomeWorldCheck = this.HomeWorldCheck.bind(this)
-
-    }
-
-    HomeWorldCheck(HWName,id){
-        id == 1 ?
-        this.setState({
-            HomeWorld1: HWName
-        }) :
-        this.setState({
-            HomeWorld2: HWName
+  HomeWorldCheck(HWName, id) {
+    id == 1
+      ? this.setState({
+          HomeWorld1: HWName
         })
-        if(this.state.HomeWorld1 && this.state.HomeWorld2){this.HomeWorldCompare()}
+      : this.setState({
+          HomeWorld2: HWName
+        });
+    if (this.state.HomeWorld1 && this.state.HomeWorld2) {
+      this.HomeWorldCompare();
     }
+  }
 
-    HomeWorldCompare(){
-        this.state.HomeWorld1 == this.state.HomeWorld2 ?
-        this.setState({
-            CompareHomeWorld: 'They are from Same Home World!'
-        }) :
-        this.setState({
-            CompareHomeWorld: 'They are from Different Home World!'
+  HomeWorldCompare() {
+    this.state.HomeWorld1 == this.state.HomeWorld2
+      ? this.setState({
+          CompareHomeWorld: "They are from Same Home World!"
         })
-    }
-    
-    render(){
-        return(
-            <div>
-                <ApiComponent 
-                id = "1" 
-                HWCheck = {this.HomeWorldCheck}
-                />
-                <ApiComponent 
-                id = "2"
-                HWCheck = {this.HomeWorldCheck}
-                />
-                <span>
-                    HW1: {this.state.HomeWorld1}
-                    <br/>
-                    HW2: {this.state.HomeWorld2}
-                    <h2>{this.state.CompareHomeWorld}</h2>
-                </span>
-            </div>
-        )
-    }
+      : this.setState({
+          CompareHomeWorld: "They are from Different Home World!"
+        });
+  }
 
+  render() {
+    return (
+      <div>
+        <div style={appStyle}>
+          <ApiComponent id="1" HWCheck={this.HomeWorldCheck} />
+          <ApiComponent id="2" HWCheck={this.HomeWorldCheck} />
+        </div>
+
+        <p style={comparisonStyle}>
+          <h2>{this.state.CompareHomeWorld}</h2>
+        </p>
+      </div>
+    );
+  }
 }
-export default App
+
+const appStyle = {
+  display: "flex",
+  justifyContent: "center"
+};
+const comparisonStyle = {
+  textAlign: "center"
+};
+export default App;
